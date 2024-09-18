edc79290d6c0c9459e88abe08d764d129f6637ec
DESKTOP-LH4O20N\PC <kircanski.bojan@gmail.com>
2024-09-18 17:51:16 +0200

    Implemented look for old rides for user

---
+++ front/BasVasTaxi-front/src/pages/OldRidesPage.tsx
@@ -0,0 +1,11 @@
+import OldRidesComponent from "../components/user/OldRidesComponent.tsx";
+
+const OldRidesPage=()=>{
+    return (
+        <>
+            <OldRidesComponent/>
+        </>
+    )
+};
+
+export default OldRidesPage;
